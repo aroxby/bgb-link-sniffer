409010474038735b706b89ee155080e93b61a7fb
Andy Roxby <aroxby@users.noreply.github.com>
2020-06-20 03:27:02 -0400

    Use command line arguments

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import sys
+import argparse
+
 from server import MessageRelayServer
 
 
@@ -6,14 +9,49 @@ def text_formatter(data):
     return data.decode('utf-8')
 
 
-def main():
-    listen_address = ('127.0.0.1', 8080)
-    upstream_address = ('www.example.com', 80)
+class AddressArgAction(argparse.Action):
+    @staticmethod
+    def parse_address(addr):
+        host, port = addr.split(':')
+        port = int(port)
+        return host, port
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        try:
+            address = self.parse_address(values)
+        except ValueError:
+            parser.error('Invalid address "{}", use the HOST:PORT form'.format(values))
+        setattr(namespace, self.dest, address)
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description='Sniff TCP Data')
+    parser.add_argument(
+        '--listen',
+        action=AddressArgAction,
+        help='Listen on this address (host:port)',
+        metavar='LISTEN_ADDRESS',
+        required=True,
+    )
+    parser.add_argument(
+        '--connect',
+        action=AddressArgAction,
+        help='Relay data to this address (host:port)',
+        metavar='UPSTREAM_ADDRESS',
+        required=True,
+    )
+
+    args = parser.parse_args(argv)
+    return args
+
+
+def main(argv):
+    args = parse_args(argv[1:])
     with MessageRelayServer(
-        listen_address, upstream_address, text_formatter
+        args.listen, args.connect, text_formatter
     ) as server:
         server.serve_forever()
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
