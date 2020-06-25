637146971f5c0a77d50ae3684901838220e66435
Andy Roxby <aroxby@users.noreply.github.com>
2020-06-25 14:58:32 -0400

    Rewrite BGB objects to be based around link values

--- bgb.py
+++ bgb.py
@@ -12,21 +12,38 @@ class BGBMessage(object):
 
     SIZE = 8
     _STRUCT_FORMAT = '=BBBBL'
+    CMD_MASTER_SYNC = 104
+    CMD_SLAVE_SYNC = 105
 
-    def __init__(self, data):
-        msg = struct.unpack(self._STRUCT_FORMAT, data)
-        self.b1, self.b2, self.b3, self.b4, self.li = msg
+    @classmethod
+    def unpack(cls, data):
+        values = struct.unpack(cls._STRUCT_FORMAT, data)
+        return cls(*values)
+
+    @classmethod
+    def for_value(cls, value):
+        return cls(cls.CMD_SLAVE_SYNC, value, 0x80, 0, 0)
+
+    def __init__(self, b1, b2, b3, b4, i1):
+        self.b1 = b1
+        self.b2 = b2
+        self.b3 = b3
+        self.b4 = b4
+        self.i1 = i1
 
     def __str__(self):
-        return str((self.b1, self.b2, self.b3, self.b4, self.li))
+        return str((self.b1, self.b2, self.b3, self.b4, self.i1))
 
-    def is_interesting(self):
-        # Used to skip some packets while debugging
-        return self.b1 not in [101, 106]
+    def get_value(self):
+        if self.b1 in [self.CMD_MASTER_SYNC, self.CMD_SLAVE_SYNC]:
+            value = self.b2
+        else:
+            value = None
+        return value
 
     def to_data(self):
         data = struct.pack(
-            self._STRUCT_FORMAT, self.b1, self.b2, self.b3, self.b4, self.li
+            self._STRUCT_FORMAT, self.b1, self.b2, self.b3, self.b4, self.i1
         )
         return data
 
@@ -40,7 +57,7 @@ class BGBChannel(object):
         while not self.closed and ready_to_read(self.socket):
             data = self.socket.recv(BGBMessage.SIZE)
             if data:
-                yield BGBMessage(data)
+                yield BGBMessage.unpack(data)
             else:
                 self.closed = True
 
@@ -54,17 +71,23 @@ class BGBRelayHandler(StreamRequestHandler):
         with TCPClient(self.server.upstream_address) as upstream_socket:
             upstream = BGBChannel(upstream_socket)
             while not client.closed and not upstream.closed:
+                client_values = []
                 for client_msg in client.recv_messages():
-                    if client_msg.is_interesting():
-                        print("{}:{} wrote: ".format(*self.client_address))
-                        print(client_msg)
+                    client_value = client_msg.get_value()
+                    if client_value is not None:
+                        client_values.append(client_value)
                     upstream.send_message(client_msg)
+                if client_values:
+                    print('Client write: {}'.format(client_values))
 
+                upstream_values = []
                 for upstream_msg in upstream.recv_messages():
-                    if upstream_msg.is_interesting():
-                        print("{}:{} wrote: ".format(*self.server.upstream_address))
-                        print(upstream_msg)
+                    upsteam_value = upstream_msg.get_value()
+                    if upsteam_value is not None:
+                        upstream_values.append(upsteam_value)
                     client.send_message(upstream_msg)
+                if upstream_values:
+                    print('Upstream write: {}'.format(upstream_values))
 
 
 class BGBRelayServer(ForkingTCPServer):
